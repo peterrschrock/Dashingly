dd097e76e0fbe7d8b171fdb40424319e38047eff
Peter Schrock <peterrschrock@gmail.com>
2017-01-09 19:20:03 -0800

    store working, reducers

--- frontend/dashingly.jsx
+++ frontend/dashingly.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {login, signup, logout} from './util/session_api_util.js';
+import configureStore from './store/store';
 
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Testing...</h1>, root);
+  const store = configureStore();
   window.login = login;
   window.signup = signup;
   window.logout = logout;
+  window.store = store;
 });
--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,20 +1,21 @@
 import {RECEIVE_ERRORS, RECEIVE_CURRENT_USER} from '../actions/session_actions';
 import lodash from 'lodash';
 
-const emptyVessel = {
-  username: null
-  errors: [];
-}
+const _emptyVessel = {
+  username: null,
+  errors: []
+};
 
-export const SessionReducer = (state = emptyVessel, action) => {
+const SessionReducer = (state = _emptyVessel, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
-      return lodash.merge({}, emptyVessel, action.user)
+      return lodash.merge({}, _emptyVessel, action.user);
     case RECEIVE_ERRORS:
-      return lodash.merge({}, emptyVessel, action.errors)
+      return lodash.merge({}, _emptyVessel, action.errors);
     default:
       return state;
-
   }
-}
+};
+
+export default SessionReducer;
