e226268bc25158e6b944b6f1d8d63ffa359b2b30
Peter Schrock <peterrschrock@gmail.com>
2017-01-11 11:47:08 -0800

    revert

---
+++ frontend/components/my_charts/dummy_chart.jsx
@@ -0,0 +1,24 @@
+// 
+// const D3-Basic = require('react-d3-basic')
+//
+// (function() {
+//   let
+// })
+//
+// <Chart
+//     title={"Taiwan refuse disposal"}
+//     width={500}
+//     height={300}
+//     margins= {margins}
+//     >
+//     <LineChart
+//       margins= {margins}
+//       title={"Taiwan refuse disposal"}
+//       data={data}
+//       width={500}
+//       height={300}
+//       chartSeries={chartSeries}
+//       x={x}
+//       xScale={"time"}
+//     />
+//   </Chart>
