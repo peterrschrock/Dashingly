fae48ddf1c0501375d115caf376cacf749a36894
Peter Schrock <peterrschrock@gmail.com>
2017-01-17 11:47:55 -0800

    Line charts functional

--- frontend/components/chart/chart_element.jsx
+++ frontend/components/chart/chart_element.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {bindAll} from 'lodash';
-import {ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+import {LineChart, Line, ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
 
 class ChartElement extends React.Component {
   constructor(props) {
     super(props);
-    bindAll(this, 'dataKey', 'rowKey', 'columnKey', 'completeChart', 'renderChart');
+    bindAll(this, 'dataKey', 'rowKey', 'columnKey', 'completeChart', 'renderChart', 'renderScatterChart', 'renderLineChart');
   }
 
   dataKey(){
@@ -31,21 +31,43 @@ class ChartElement extends React.Component {
     return outcome;
   }
 
-  renderChart(){
-    if(this.completeChart()) {
-      return <ScatterChart width={400} height={400} margin={{top: 20, right: 20, bottom: 20, left: 20}}>
-        	<XAxis dataKey={this.rowKey()} name='stature' unit='cm'/>
-        	<YAxis dataKey={this.columnKey()} name='weight' unit='kg'/>
-        	<Scatter name='A school' data={this.dataKey()} fill='#8884d8'/>
-        	<CartesianGrid />
-        	<Tooltip cursor={{strokeDasharray: '3 3'}}/>
-        </ScatterChart>;
-      } else {
-          return <h2> Not Enough Data</h2>;
-      }
+  renderScatterChart(){
+    return <ScatterChart width={600} height={400} margin={{top: 20, right: 20, bottom: 20, left: 20}}>
+      <XAxis dataKey={this.rowKey()} name='stature' unit='cm'/>
+      <YAxis dataKey={this.columnKey()} name='weight' unit='kg'/>
+      <Scatter name='A school' data={this.dataKey()} fill='#8884d8'/>
+      <CartesianGrid />
+      <Tooltip cursor={{strokeDasharray: '3 3'}}/>
+    </ScatterChart>;
   }
 
+  renderLineChart(){
+    return <LineChart width={600} height={400} data={this.dataKey()}
+            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+     <XAxis dataKey={this.rowKey()}/>
+     <YAxis dataKey={this.columnKey()}/>
+     <CartesianGrid strokeDasharray="3 3"/>
+     <Tooltip/>
+     <Legend />
+     <Line type="monotone" dataKey={this.columnKey()} stroke="#8884d8" activeDot={{r: 8}}/>
+    </LineChart>;
+  }
 
+  renderChart(){
+    // debugger
+    if(this.completeChart()) {
+      switch (this.props.chartNewState.chartType) {
+        case "SCATTER":
+          return this.renderScatterChart();
+        case "LINE":
+          return this.renderLineChart();
+        default:
+          return <h2> Not Enough Data</h2>;
+      }
+    } else {
+        return <h2> Not Enough Data</h2>;
+    }
+  }
 
   render(){
     return this.renderChart();
